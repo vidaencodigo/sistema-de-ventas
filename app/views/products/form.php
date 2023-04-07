0fc4915f32dd5b9ff0de676edb9848c2f181ed7a
Emmanuel <emmanuel.lucio.urbina@outlook.com>
2023-04-07 02:31:30 -0500

    css

--- app/views/products/form.php
+++ app/views/products/form.php
@@ -7,32 +7,60 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ventas | sistema de punto de venta web</title>
     <link rel="stylesheet" href="../app/assets/css/styles.css">
+
+    
 </head>
 
-<body>
-    <nav class="nav">
-        <div class="brand">
-            Ventas
-        </div>
-        <div class="nav--container">
-            <div class="menu--nav">
-                <div class="nav--item">
-                    <a href="#" class="nav--link">
-                        Inicio
-                    </a>
-                </div>
 
-            </div>
-        </div>
-    </nav>
+<body>
+
 
     <div class="app">
         <section class="side">
+            <div class="side--content">
+
+                <div class="brand">
+                    Ventas
+                </div>
+                <div class="nav">
+                    <div class="link--item">
+                        <a href="#" class="item--link">Inicio</a>
+                    </div>
+                    <div class="link--item">
+                        <a href="#" class="item--link">Productos</a>
+                    </div>
+                </div>
+            </div>
+
 
         </section>
         <section class="main">
+            <article class="main--header">
+                <div>
+                    <a href="#" class="btn sell">
+                        Nueva venta
+                    </a>
+                </div>
+            </article>
+            <article class="main--content">
 
-
+                <div class="div--form">
+                    <div class="div--form--header">
+                        <p>Nuevo Producto</p>
+                    </div>
+                    <div class="div--form--content">
+                        <p>
+                            Información de producto
+                        </p>
+                        <form class="form">
+                            <div class="group--form">
+                                <label for="code">Código</label>
+                                <input type="text" class="input" placeholder="Código...">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </article>
         </section>
 
     </div>
